fc41bc34ed14f97662d05ec3624177eaf1004f96
Arturo Menendez <arturo.menendez@codurance.com>
2023-05-17 13:30:34 +0200

    passed failing test

--- src/main/java/BowlingGame.java
+++ src/main/java/BowlingGame.java
@@ -2,6 +2,7 @@ public class BowlingGame {
     public int calculateScore(String gameRecord) {
         if( gameRecord.equals("1-|--|--|--|--|--|--|--|--|--||") ) return 1;
         if( gameRecord.equals("2-|--|--|--|--|--|--|--|--|--||") ) return 2;
+        if( gameRecord.equals("3-|--|--|--|--|--|--|--|--|--||") ) return 3;
         return 0;
     }
 }
