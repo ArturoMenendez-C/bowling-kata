e30003805a67429e40f5f691a3e8d75800ef7793
K Leonardo Ruiz Flórez <kleonardoruizflorez@gmail.com>
2023-05-17 15:55:54 +0200

    Refactored multiple cases of first roll into a parameterized test case

--- src/test/java/BowlingGameShould.java
+++ src/test/java/BowlingGameShould.java
@@ -1,8 +1,22 @@
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BowlingGameShould {
+
+    private static Stream<Arguments> firstRollPinsDownCase () {
+        String firstRollFormat = "-|--|--|--|--|--|--|--|--|--||";
+        return Stream.of(
+                Arguments.of(1, "1" + firstRollFormat),
+                Arguments.of(2, "2" + firstRollFormat),
+                Arguments.of(3, "3" + firstRollFormat)
+        );
+    }
 
     @Test
     void score_is_zero_when_no_pins_down_in_the_whole_game() {
@@ -14,37 +28,15 @@ public class BowlingGameShould {
         assertEquals(expectedScore, gameScore);
     }
 
-    @Test
-    void score_is_one_when_one_pin_down_in_the_whole_game() {
+    @ParameterizedTest
+    @MethodSource("firstRollPinsDownCase")
+    void score_is_one_when_one_pin_down_in_the_whole_game(int points, String gameRecord) {
         BowlingGame bowlingGame = new BowlingGame(new Score());
         Score expectedScore = new Score();
 
-        expectedScore.addPoints(1);
-        Score gameScore = bowlingGame.calculateScore("1-|--|--|--|--|--|--|--|--|--||");
+        expectedScore.addPoints(points);
+        Score gameScore = bowlingGame.calculateScore(gameRecord);
 
         assertEquals(expectedScore, gameScore);
     }
-
-    @Test
-    void score_is_two_when_two_pins_down_in_the_whole_game() {
-        BowlingGame bowlingGame = new BowlingGame(new Score());
-        Score expectedScore = new Score();
-
-        expectedScore.addPoints(2);
-        Score gameScore = bowlingGame.calculateScore("2-|--|--|--|--|--|--|--|--|--||");
-
-        assertEquals(expectedScore, gameScore);
-    }
-
-    @Test
-    void score_is_three_when_three_pins_down_in_the_whole_game() {
-        BowlingGame bowlingGame = new BowlingGame(new Score());
-        Score expectedScore = new Score();
-
-        expectedScore.addPoints(3);
-        Score gameScore = bowlingGame.calculateScore("3-|--|--|--|--|--|--|--|--|--||");
-
-        assertEquals(expectedScore, gameScore);
-    }
-
 }
