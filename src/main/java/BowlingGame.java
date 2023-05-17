3af38dc6542f868767a3fd13d5294cd2fe91912e
Arturo Menendez <arturo.menendez@codurance.com>
2023-05-17 13:53:52 +0200

    refactor to improve readability, semantics and structure

--- src/main/java/BowlingGame.java
+++ src/main/java/BowlingGame.java
@@ -1,8 +1,18 @@
 public class BowlingGame {
     public int calculateScore(String gameRecord) {
-        if( gameRecord.equals("1-|--|--|--|--|--|--|--|--|--||") ) return 1;
-        if( gameRecord.equals("2-|--|--|--|--|--|--|--|--|--||") ) return 2;
-        if( gameRecord.equals("3-|--|--|--|--|--|--|--|--|--||") ) return 3;
-        return 0;
+//        if( gameRecord.equals("1-|--|--|--|--|--|--|--|--|--||") ) return 1;
+//        if( gameRecord.equals("2-|--|--|--|--|--|--|--|--|--||") ) return 2;
+//        if( gameRecord.equals("3-|--|--|--|--|--|--|--|--|--||") ) return 3;
+//        return 0;
+
+        int score = 0;
+
+        String firstRoll = gameRecord.split("")[0];
+
+        if(!firstRoll.equals("-")){
+            score = Integer.parseInt(firstRoll);
+        }
+
+        return score;
     }
 }
